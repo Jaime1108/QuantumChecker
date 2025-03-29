bdaa11a7cf2e57888dc2458f1007a06b57bf8a7b
Khoa Nguyen <166854304+Jaime1108@users.noreply.github.com>
2025-03-29 11:56:22 -0500

    update

--- app/src/main/java/com/example/codequantum_project/GameplayActivity.java
+++ app/src/main/java/com/example/codequantum_project/GameplayActivity.java
@@ -8,14 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {   //user interface start screen
+public class GameplayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_gameplay);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/codequantum_project/Model/Checkerboard.java
@@ -0,0 +1,4 @@
+package com.example.codequantum_project.Model;
+
+public class Checkerboard {
+}
